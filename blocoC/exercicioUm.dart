af84af60287e993b1eac273356f262dda4c8c5ac
Dheniel Rodrigues Luis <dheniel.luis@estudante.ifgoiano.edu.br>
2026-08-19 20:22:11 -0300

    Update exercicioUm.dart

---
+++ blocoC/exercicioUm.dart
@@ -0,0 +1,7 @@
+String primeiraParte = "O Talhão 3 tem 42,0";
+String segundaParte = " ha plantados";
+String terceiraParte = " com milho.";
+
+void main(){
+  print(primeiraParte + segundaParte + terceiraParte);
+}
